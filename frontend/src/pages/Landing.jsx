e16eee985f3837edbb5996d2648ae946ef98cce9
Raj <ranadkar@ucsc.edu>
2024-08-28 13:58:03 -0700

    navigation business

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,7 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
 const Landing = () => {
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/login');
+  }
+
+  const handleRegister = () => {
+    navigate('/register');
+  }
+
+  const handleGuest = () => {
+    navigate('/game');
+  }
+
   return (
     <div>
       <h1>Hello World</h1>
+      <button onClick={(handleLogin)}>Login</button>
+      <button onClick={(handleRegister)}>Register</button>
+      <button onClick={(handleGuest)}>Play As Guest</button>
     </div>
   );
 };
