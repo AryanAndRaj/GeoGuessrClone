d8bdee794c106a5eceb93620b717350cbe1fee89
Aryan Vora <voraaryan1@gmail.com>
2024-08-30 14:08:01 -0700

    allow users to play as guest from auth page

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -3,39 +3,51 @@ import PropTypes from "prop-types";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
 import styles from "./Auth.module.css";
+
 const AuthForm = ({
   isLogin,
   username,
   setUsername,
   password,
   setPassword,
+  setConfirmPassword,
+  confirmPassword,
   handleSubmit,
   message,
-}) => (
-  <form onSubmit={handleSubmit}>
-    <h1>{isLogin ? "Sign In" : "Create Account"}</h1>
-    <span>
-      {isLogin
-        ? "Login With Username & Password"
-        : "Register with Username & Password"}
-    </span>
-    <input
-      type="text"
-      placeholder="Username"
-      value={username}
-      onChange={(e) => setUsername(e.target.value)}
-    />
-    <input
-      type="password"
-      placeholder="Password"
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-    />
-    {message && <div className={styles.message}>{message}</div>}
-
-    <button type="submit">{isLogin ? "Sign In" : "Sign Up"}</button>
-  </form>
-);
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>{isLogin ? "Sign In" : "Create Account"}</h1>
+      <span>
+        {isLogin
+          ? "Login With Username & Password"
+          : "Register with Username & Password"}
+      </span>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      {!isLogin && (
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+      )}
+      {message && <div className={styles.message}>{message}</div>}
+      <button type="submit">{isLogin ? "Sign In" : "Sign Up"}</button>
+    </form>
+  );
+};
 
 AuthForm.propTypes = {
   isLogin: PropTypes.bool.isRequired,
@@ -43,26 +55,35 @@ AuthForm.propTypes = {
   setUsername: PropTypes.func.isRequired,
   password: PropTypes.string.isRequired,
   setPassword: PropTypes.func.isRequired,
+  confirmPassword: PropTypes.string,
+  setConfirmPassword: PropTypes.func,
   handleSubmit: PropTypes.func.isRequired,
   message: PropTypes.string.isRequired,
+  handleGuestNavigate: PropTypes.func.isRequired,
 };
 
 export default function Auth() {
   const [isLogin, setIsLogin] = useState(true);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [message, setMessage] = useState("");
   const navigate = useNavigate();
   const handleSwitch = () => {
     setIsLogin(!isLogin);
     setMessage("");
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const url = isLogin
       ? "http://localhost:5000/login"
       : "http://localhost:5000/register";
 
+    if (!isLogin && password !== confirmPassword) {
+      setMessage("Passwords do not match");
+      return;
+    }
     try {
       const response = await axios.post(url, {username, password});
 
@@ -93,6 +114,8 @@ export default function Auth() {
             setUsername={setUsername}
             password={password}
             setPassword={setPassword}
+            confirmPassword={confirmPassword}
+            setConfirmPassword={setConfirmPassword}
             handleSubmit={handleSubmit}
             message={message}
           />
@@ -112,17 +135,35 @@ export default function Auth() {
         <div className={styles.toggleContainer}>
           <div className={styles.toggle}>
             <div className={`${styles.togglePanel} ${styles.toggleLeft}`}>
-              <h1>Login yap here!</h1>
-              <p>yapyapyapyap</p>
+              <h1>Login here!</h1>
+              <p>
+                Login to see your scores and access the multiplayer leaderboard!
+              </p>
               <button className={styles.hidden} onClick={() => handleSwitch()}>
                 Sign In
               </button>
+              <p className={styles.or}>or</p>
+              <button
+                className={styles.guestButton}
+                onClick={() => navigate("/game")}
+              >
+                Continue as guest
+              </button>
             </div>
             <div className={`${styles.togglePanel} ${styles.toggleRight}`}>
-              <h1>Register yap here!</h1>
-              <p>yapyapyapyap</p>
+              <h1>Register here!</h1>
+              <p>
+                Registering allows you to save scores to the global leaderboard!
+              </p>
               <button className={styles.hidden} onClick={() => handleSwitch()}>
                 Sign Up
+              </button>
+              <p className={styles.or}>or</p>
+              <button
+                className={styles.guestButton}
+                onClick={() => navigate("/game")}
+              >
+                Continue as guest
               </button>
             </div>
           </div>
